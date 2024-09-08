af995104af5080bfa7e7cccc1e848af925831e77
Roshan <itsroshanksingh@gmail.com>
2024-09-07 23:15:43 -0500

    Removed user app

--- apps/users/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsersConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-
-    # user app in apps folder
-    name = "apps.users"
